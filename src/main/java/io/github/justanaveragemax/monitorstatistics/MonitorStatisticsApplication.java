773c1714b531377ace68d9167feb05f9cf2ef517
Maksim Krutalevich <m.krutalevich@kiwiride.com>
2025-06-17 21:45:15 +0300

    Initial commit

---
+++ src/main/java/io/github/justanaveragemax/monitorstatistics/MonitorStatisticsApplication.java
@@ -0,0 +1,13 @@
+package io.github.justanaveragemax.monitorstatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorStatisticsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MonitorStatisticsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/io/github/justanaveragemax/monitorstatistics/MonitorStatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.justanaveragemax.monitorstatistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorStatisticsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
